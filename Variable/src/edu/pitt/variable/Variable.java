0affe10587f20097fa4e0695b863cb551aee41bc
r3xley <78571570+r3xley@users.noreply.github.com>
2022-09-01 10:17:15 -0400

    cmpinf401

---
+++ Variable/src/edu/pitt/variable/Variable.java
@@ -0,0 +1,12 @@
+package edu.pitt.variable;
+import javax.swing.JOptionPane;
+public class Variable {
+
+	public static void main(String[] args) {
+		String name = "Robert Exley";
+		System.out.println(name + "lol");
+		
+
+	}
+
+}
