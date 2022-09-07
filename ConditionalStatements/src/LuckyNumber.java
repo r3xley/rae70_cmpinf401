15441268372c884dbcde26996362a5bbe0dd06f1
r3xley <78571570+r3xley@users.noreply.github.com>
2022-09-07 10:39:26 -0400

    class examples

--- ConditionalStatements/src/LuckyNumber.java
+++ ConditionalStatements/src/LuckyNumber.java
@@ -1,9 +1,20 @@
+import javax.swing.JOptionPane;
 
 public class LuckyNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String userinput = JOptionPane.showInputDialog("Please enter a number");
+		
+		int userInputInt = Integer.parseInt(userinput);
+		
+		if(userInputInt == 7) {
+			System.out.println("Lucky number");
+			
+		}
+		else {
+			System.out.println("Pick another line");
+		}
+		
 	}
 
 }
---
+++ ConditionalStatements/src/trash.java
@@ -0,0 +1,9 @@
+
+public class trash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
