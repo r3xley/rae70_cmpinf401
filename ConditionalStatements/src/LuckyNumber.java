94e1d01f64ae646835d8c24f91aeb93f0abf2ace
r3xley <78571570+r3xley@users.noreply.github.com>
2022-10-17 12:15:18 -0400

    lab 6

--- ConditionalStatements/src/LuckyNumber.java
+++ ConditionalStatements/src/LuckyNumber.java
@@ -3,14 +3,13 @@ import javax.swing.JOptionPane;
 public class LuckyNumber {
 
 	public static void main(String[] args) {
-		String input = JOptionPane.showInputDialog("Temp");
-		int temp = Integer.parseInt(input);
-		
-		if(temp>100)
-		{System.out.print(false);
-		}else {
-			System.out.print(t
+		int result = 5;
+		for (int i = result; i > 0; i--){
+		result = i + i;
+		System.out.print(i);
 		}
+		
+		
 		
 	
 	}
