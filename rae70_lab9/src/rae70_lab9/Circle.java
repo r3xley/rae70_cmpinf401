7813f75e9036a09cd650ff6b47bbac61ad83ca1d
r3xley <78571570+r3xley@users.noreply.github.com>
2022-11-13 16:35:00 -0500

    lab 9

--- rae70_lab9/src/rae70_lab9/Circle.java
+++ rae70_lab9/src/rae70_lab9/Circle.java
@@ -1,5 +1,32 @@
 package rae70_lab9;
 
 public class Circle {
-
+    private double radius;
+	private String color;
+	
+	public Circle() {
+		this.radius = 5.0;
+		this.color = "Green";
+	}
+	
+	public Circle(double radius) {
+		this.radius = radius;
+		
+	}
+	
+	public Circle(String color, double radius) {
+		this.color = color;
+		this.radius = radius;
+	}
+	
+	public double getRadius() {
+		return this.radius;
+	}
+	public String getColor() {
+		return this.color;
+		
+	}
+	public double getArea() {
+		return radius * radius * Math.PI;
+	}
 }
