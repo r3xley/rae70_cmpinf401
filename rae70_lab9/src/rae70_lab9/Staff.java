f69a74e5210f524ae4b7d7a2484e7eac81883dcf
r3xley <78571570+r3xley@users.noreply.github.com>
2022-11-14 15:39:16 -0500

    9

---
+++ rae70_lab9/src/rae70_lab9/Staff.java
@@ -0,0 +1,14 @@
+package rae70_lab9;
+
+
+public class Staff extends Person {
+    private String school;
+    private double pay;
+
+public Staff(String name, String address, double pay){
+
+}
+
+    
+    
+}
